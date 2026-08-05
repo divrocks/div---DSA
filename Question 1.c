a3079d10f47f12f801da39e14e42519defa8d0b4
divrocks <kvb4244t8b@privaterelay.appleid.com>
2026-08-05 08:29:35 +0530

    Add files via upload

---
+++ Question 1.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+
+int main()
+{
+	int a[100],i,j,n,temp,rotate;
+
+	printf("enter size of array : \n");
+	scanf("%d",&n);
+    
+
+	printf("enter elements of array : \n");
+	for(i=0; i<n; i++) {
+		scanf("%d",&a[i]);
+	}
+
+	printf("elements of array : \n");
+	for(i=0; i<n; i++) {
+		printf("%d ",a[i]);
+	}
+
+	printf("\nenter no of rotations : \n");
+	scanf("%d",&rotate);
+
+	for(j=0; j<rotate; j++) {
+		temp=a[0];
+		for(i=0; i<n-1; i++) {
+			a[i]=a[i+1];
+		}
+		a[n-1]=temp;
+	}
+
+	printf("rotated elements of array are : \n");
+	for(i=0; i<n; i++) {
+		printf("%d ",a[i]);
+	}
+
+	return 0;
+}
