bb61c7d3c51ffd6bfd87884927857ad4c44ab7ec
divrocks <kvb4244t8b@privaterelay.appleid.com>
2026-08-09 10:01:52 +0530

    week 1 question 2.c

--- week 1 question 2.c
+++ week 1 question 2.c
@@ -1,3 +1,7 @@
+/*given an unsorted array of integers and two numbers a and b. Design an algorithm and write a program 
+to find minimum distance between a and b in this array. Minimum distance between any two numbers a and b present in 
+array is the minimum distance between their indices.*/
+
 #include<stdio.h>
 #include<stdlib.h>
 int main() {
